d86faafd964dfadf10c57a6cb146c1617c5b8469
Acer <shaxzodbekbobobekov@gmail.com>
2023-02-26 23:35:51 +0500

    added new function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 "use strict";
-class Car {
-    constructor(brand, model, price) {
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
-    }
-    drive() {
-        return " Car is draving";
-    }
-}
-class NewCar extends Car {
-    constructor(brand, model, price, yoqilgiTuri, modelPlus) {
-        super(brand, model, price);
-        this.yoqilgiTuri = yoqilgiTuri;
-        this.modelPlus = modelPlus;
-    }
-}
-const result = new Car("GM", "Captiva", 25000);
-const result2 = new NewCar("Tesla", "X2", 75000, "ECO", "Super X2");
-console.log(result);
-console.log(result2);
+// class Car {
+//   brand: string;
+//   model: string;
+//   price: number;
+//   constructor(brand: string, model: string, price: number) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+let obj;
+obj = { name: "GM", year: 2024, model: "Qimmatidan", price: 20000 };
+console.log(obj);
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,37 @@
-class Car {
-  brand: string;
-  model: string;
-  price: number;
-  constructor(brand: string, model: string, price: number) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-  }
+// class Car {
+//   brand: string;
+//   model: string;
+//   price: number;
+//   constructor(brand: string, model: string, price: number) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
 
-  drive() {
-    return " Car is draving";
-  }
-}
+//   drive() {
+//     return " Car is draving";
+//   }
+// }
 
-class NewCar extends Car {
-  yoqilgiTuri: string;
-  modelPlus: string;
-  constructor(brand: string, model: string, price: number,yoqilgiTuri: string, modelPlus: string) {
-    super(brand, model, price);
-    this.yoqilgiTuri = yoqilgiTuri;
-    this.modelPlus = modelPlus; 
-  }
-}
+// class NewCar extends Car {
+//   yoqilgiTuri: string;
+//   modelPlus: string;
+//   constructor(brand: string, model: string, price: number,yoqilgiTuri: string, modelPlus: string) {
+//     super(brand, model, price);
+//     this.yoqilgiTuri = yoqilgiTuri;
+//     this.modelPlus = modelPlus;
+//   }
+// }
 
-const result = new Car("GM", "Captiva", 25000);
-const result2 = new NewCar("Tesla", "X2", 75000, "ECO", "Super X2");
+// const result = new Car("GM", "Captiva", 25000);
+// const result2 = new NewCar("Tesla", "X2", 75000, "ECO", "Super X2");
 
-console.log(result);
-console.log(result2);
+// console.log(result);
+// console.log(result2);
+
+type OBJ = {name: string, year:number, model: string, price:number}
+let obj:OBJ
+
+obj = {name:"GM",year: 2024, model: "Qimmatidan", price: 20_000};
+
+console.log(obj)
