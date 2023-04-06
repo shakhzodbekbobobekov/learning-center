b6f1396707e1a7b13efcb87e5c9a158491ec437e
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2023-04-06 17:15:00 +0500

    added tag, text, heading components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Heading, Tag, Text } from "@/components";
 
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div>
+      <Heading tag="h3">Children</Heading>
+      <Text size="s" >Text</Text>
+      <Tag size="s" color="red" > Size s - Color red</Tag>
+    </div>
+  );
 };
 
 export default Index;
